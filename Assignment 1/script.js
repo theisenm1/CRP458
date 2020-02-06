4f9bf2eb6b5dd45a5df77472d9b7fce0131a5221
Matt Theisen <theisenm@iastate.edu>
2020-02-06 08:53:18 -0600

    Assignment 1a
    deleted exercise4 and added 4a
    
    deleted old assignment1.css and created new assignment 1.js and index.html files

---
+++ Assignment 1/script.js
@@ -0,0 +1,13 @@
+
+function findLongestWord() {
+    a<b ? console.log("a: ", a) : console.log("b: ", b)
+
+}
+
+var a = "word";
+var b = "Pencil";
+
+findLongestWord();
+
+var names = newArray("Steve", "Leah", "Jeff", "Ben", "Jeanne");
+document.getElementById("names").innerHTML = names;
